2d812ee8b0640f69ac9eb1b0eade93def94d92ad
ivshynalarysa <ivshynalarysa@gmail.com>
2025-02-27 10:47:00 +0000

    add task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,15 @@
+const colorGeneral = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  colorGeneral.textContent = color;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
